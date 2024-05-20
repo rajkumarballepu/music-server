034bb346c69430dca493b935635f531426ee3f2a
Raj Kumar Ballepu <108982663+rajkumarballepu@users.noreply.github.com>
2024-05-20 12:44:19 +0530

    serverChange

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = app.listen(5000, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://192.168.0.184:5000",
+    origin: "http:localhost:5000",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
