0f10fdc42780cd29bf26b6f42b1007860f5ed283
Raj Kumar Ballepu <108982663+rajkumarballepu@users.noreply.github.com>
2024-05-20 11:56:15 +0530

    ver1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+const server = app.listen(5000, () => {
+  console.log("Server started on 5000");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://192.168.0.184:5000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("joinRoom", (data) => {
+    socket.join(data.roomId);
+  });
+  socket.on("send", (data) => {
+    console.log(data);
+    socket.to(data.roomId).emit("recieve", { message: "hai there" });
+  });
+
+  socket.on("playSong", (data) => {
+    console.log(data);
+    socket.to(data.roomId).emit("playOnClient", data);
+  });
+
+  socket.on("pauseSong", (data) => {
+    socket.to(data.roomId).emit("pauseOnClient", data);
+  });
+});
