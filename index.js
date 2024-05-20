9a170ae4b7fef36f9033d0459dab41b71c95eb15
Raj Kumar Ballepu <108982663+rajkumarballepu@users.noreply.github.com>
2024-05-20 12:52:11 +0530

    serverChange2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = app.listen(5000, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http:localhost:5000",
+    origin: "https://music-server-rtgy.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
